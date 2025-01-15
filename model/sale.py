dd28cba237ef8c5e979ca9255a2b56f79b0a41ed
palaciosyost <palaciosyostin903@gmail.com>
2025-01-15 15:56:21 -0500

    agregando funcionalidad de cambio de precio (bloqueo)

--- model/sale.py
+++ model/sale.py
@@ -27,11 +27,16 @@ class SaleDescuento(models.Model):
                             f"El descuento {line.discount}% del producto '{line.product_id.name}' excede el "
                             f"descuento máximo permitido en la categoría '{line.product_template_id.categ_id.name}'."
                         )
-                    if line.product_template_id.detailed_type == 'product':
-                        if line.price_unit != line.product_template_id.list_price:
-                             if line.product_id.id != line.product_template_id.id:
-                                line.price_unit = line.product_template_id.list_price
-                                raise UserError(
-                                    f"Usted no tiene permitido cambiar el precio establecido en el producto '{line.product_id.name}' "
-                                )
                     
+                    
+    def uodate_price_product(self):
+        for record in self:
+            for line in record.order_line:
+                if line.product_template_id.detailed_type == 'product':
+                    if line.price_unit != line.product_template_id.list_price:
+                        raise UserError(
+                            f"Usted no tiene permitido cambiar el precio establecido en el producto '{line.product_id.name}' "
+                        )
+                        line.write({
+                            'price_unit' : line.product_template_id.list_price
+                        })
