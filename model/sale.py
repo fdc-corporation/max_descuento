d7d3c829dcbe071cc0df5da891d4a41a05b603c1
palaciosyost <palaciosyostin903@gmail.com>
2025-01-15 17:12:17 -0500

    agregando funcionalidad de cambio de precio (bloqueo)

--- model/sale.py
+++ model/sale.py
@@ -9,40 +9,42 @@ class SaleDescuento(models.Model):
         # Verifica si hay cambios en las líneas de pedido
         if 'order_line' in vals:
             self.max_descuento()
-            # self.uodate_price_product()
+            self.update_price_product()  # Asegúrate de ejecutar la actualización después de los cambios
         return res
 
     def create(self, vals):
         res = super(SaleDescuento, self).create(vals)
         if 'order_line' in vals:
             self.max_descuento()
-            # self.uodate_price_product()
+            self.update_price_product()  # Asegúrate de ejecutar la actualización después de los cambios
         return res
 
     def max_descuento(self):
         for record in self:
             if self.env.user.has_group('max_descuento.group_user_ventas'):
                 for line in record.order_line:
-                    decuento_max = line.product_template_id.categ_id.descuento_max or line.product_template_id.categ_id.parent_id.descuento_max
-                    if line.discount > line.product_template_id.categ_id.descuento_max:
+                    descuento_max = line.product_template_id.categ_id.descuento_max or line.product_template_id.categ_id.parent_id.descuento_max
+                    if line.discount > descuento_max:
                         raise UserError(
                             f"El descuento {line.discount}% del producto '{line.product_id.name}' excede el "
                             f"descuento máximo permitido en la categoría '{line.product_template_id.categ_id.name}'."
                         )
-                    
-                    
-    def uodate_price_product(self):
+
+    @api.depends('order_line.product_template_id')          
+    def update_price_product(self):
         for record in self:
-            for line in record.order_line:
-                if line.product_template_id.detailed_type == 'product':
-                    if line.price_unit != line.product_template_id.list_price:
-                        print("--------------------------PRECIO")
-                        print(line.product_template_id.name)
-                        print(line.price_unit)
-                        print(line.product_template_id.list_price)
-                        raise UserError(
-                            f"Usted no tiene permitido cambiar el precio establecido en el producto '{line.product_id.name}' "
-                        )
-                        line.write({
-                            'price_unit' : line.product_template_id.list_price
-                        })
+            if self.env.user.has_group('max_descuento.group_user_ventas'):
+                for line in record.order_line:
+                    
+                    # Verifica si el producto es un producto (no un servicio) y si el precio de lista es igual al precio unitario
+                    if line.product_template_id.detailed_type == 'product':
+                        if line.price_unit != line.product_template_id.list_price:
+                            # Si el precio unitario no es igual al precio de lista y el producto es nuevo, no se modifica
+                            if line.id and line.price_unit != line.product_template_id.list_price:
+                                raise UserError(
+                                    f"Usted no tiene permitido cambiar el precio establecido en el producto '{line.product_id.name}' "
+                                    f"al precio de lista de '{line.product_template_id.list_price}'."
+                                )
+                            line.write({
+                                'price_unit': line.product_template_id.list_price
+                            })
